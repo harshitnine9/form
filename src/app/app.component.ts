3cc44f94b2cb3cd61a7bf956714d6522d9ba6ac6
harshit pant <harshit.pant@quantiphi.com>
2020-09-07 00:29:53 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formapp';
+   numbersearch(){
+    document.getElementById("searchbynumber").style.visibility = "visible";
+    document.getElementById("searchbyemail").style.visibility = "hidden";
+  }
+  emailsearch(){
+    document.getElementById("searchbyemail").style.visibility = "visible";
+    document.getElementById("searchbynumber").style.visibility = "hidden";
+  }
+  onsubmit(data){
+    fetch('http://localhost:3000/data', {
+    method: 'POST',
+    body: JSON.stringify({
+      name: data.name,
+      email: data.email,
+      phone: data.number
+
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then(response => response.json())
+  .then(json => console.log(json))
+  alert('data submitted successfully to db.json')
+  }
 }
